879d868e8adbea8de2bedf46cfb9564faf94e8a6
Анна <52636876+annykay@users.noreply.github.com>
2022-02-25 17:17:31 +0300

    Update agg_stats.r

--- agg_stats.r
+++ agg_stats.r
@@ -6,10 +6,10 @@ workdir <- args[1]
 genomes <- str_split(args[2], '\n')
 
 for (genome in genomes){
-	filename <- paste(workdir, '/results_', str_replace_all(genome, '-', '_'), '/gstats_library_group/')
+	filename <- paste(workdir, '/results_', str_replace_all(genome, '-', '_'), '/gstats_library_group/all_libraries.14-41.stats', sep='')
 	varname <- paste("stats_", str_replace_all(genome, '-', '_'), sep="")
 	assign(varname, read.table(file = filename, col.names = c("name", genome)))
-
+}
 
 total <- merge(stats_14_24, stats_14_41, by = "name", all=TRUE)
 total <- merge(total, stats_X44, by = "name", all=TRUE)
@@ -17,5 +17,5 @@ total <- merge(total, stats_H4_8, by = "name", all=TRUE)
 
 total <- total[order(match(total[,1], stats_X44[,1])),]
 
-file_out <-  paste(workdir, '/aggr_results/aggr_stats.csv')
+file_out <-  paste(workdir, '/aggr_results/aggr_stats.csv', sep='')
 write.csv(total, file_out, quote=FALSE, sep="\t")

6078ba7f12e6a12f2ae4963d5b60532f2edce22f
Анна <52636876+annykay@users.noreply.github.com>
2022-02-25 17:18:47 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -10,5 +10,5 @@ do
 	./binarize.sh $GENOME $WORKDIR'/results_'$GENOME_'/gmapped_parsed_sorted_chunks/'
 done < genomes.txt
 mkdir aggr_results 
-./fisher_test $WORKDIR "$(< genomes.txt)"
+./fisher_test.r $WORKDIR "$(< genomes.txt)"
 ./agg_stats.r $WORKDIR "$(< genomes.txt)"
